18fa0176287f00d893e9eb1910f4549173f2733a
c00l9uy <c00l9uy@mail.com>
2016-02-06 13:53:11 +0800

    test

---
+++ nc.py
@@ -0,0 +1,178 @@
+#coding:utf-8
+__author__ = 'HXF2014'
+
+
+import sys
+import  socket
+import getopt
+import threading
+import subprocess
+
+#全局变量
+listen = False
+command = False
+upload = False
+execute = ''
+target = ''
+upload_destination = ''
+port = 0
+
+def usage():
+    print 'Netcat Tool'
+    print
+    print 'Usage: nc.py -t target_host -p target_port'
+    print '-l  --listen                 - listen on [host]:[port] for incoming  connections'
+    print '-e  --execute=file_to_run    - execute the given file upon receiving a connection'
+    print '-c  --command                - initialize a command shell'
+    print '-u  --upload=destination     - upon receiving connection upload a file and write to [destination]'
+    print
+    print
+    print 'Examples:'
+    print 'nc.py -t 192.168.0.1 -p 5555 -l -c'
+    print r'nc.py -t 192.168.0.1 -p 5555 -l -u c:\target.exe'
+    print r'nc.py -t 192.168.0.1 -p 5555 -l -e /cat /etc/passwd'
+    print 'echo "ABCDEFGHI" | ./nc.py -t 192.168.0.1 -p 5555'
+    sys.exit(0)
+
+
+
+
+def client_sender(buffer):
+    client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    try:
+        client.connect((target,port))
+        if len(buffer):
+            client.send(buffer)
+
+        while True:
+            recv_len = 1
+            response = ''
+
+            while recv_len:
+                data = client.recv(4096)
+                recv_len = len(data)
+                response += data
+
+                if recv_len < 4096:
+                    break
+
+            print response
+
+            buffer = raw_input('')
+            buffer += '\n'
+            client.send(buffer)
+    except:
+        print '[*] Exception! Exiting.'
+    client.close()
+
+
+
+def run_command(command):
+    command = command.rstrip()
+    try:
+        output = subprocess.check_output(command,stderr=subprocess.STDOUT,shell=True)
+    except:
+        output = 'Failed to execute command.\r\n'
+
+    return output
+
+
+
+def client_handler(client_socket):
+    if len(upload_destination):
+        file_buffer = ''
+        while True:
+            data = client_socket.recv(1024)
+            file_buffer += data
+            if data < 0:
+                break
+
+        try:
+            fh = open(upload_destination,'wb')
+            fh.write(file_buffer)
+            fh.close()
+
+            client_socket.send('Successfully saved file to %s\r\n' % upload_destination)
+        except:
+            client_socket.send('Failed to save file to %s \r\n' % upload_destination)
+
+    if len(execute):
+        output = run_command(execute)
+        client_socket.send(output)
+
+    if command:
+        while True:
+            client_socket.send('<nc:#>')
+            cmd_buffer = ''
+            while '\n' not in cmd_buffer:
+                cmd_buffer += client_socket.recv(1024)
+            response = run_command(cmd_buffer)
+            client_socket.send(response)
+
+
+
+def server_loop():
+    global target
+    if not len(target):
+        target = '0.0.0.0'
+
+    server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    server.bind((target,port))
+    server.listen(5)
+
+    while True:
+        client_socket,addr = server.accept()
+        client_thread = threading.Thread(target=client_handler,args=(client_socket,))
+        client_thread.start()
+
+
+
+def main():
+    global listen
+    global port
+    global execute
+    global command
+    global upload_destination
+    global target
+
+    if not len(sys.argv[1:]):
+        usage()
+
+    try:
+        opts,args = getopt.getopt(sys.argv[1:],'hle:ct:p:u:',['help','listen','execute=','command','target=','port=','upload='])
+    except getopt.GetoptError as err:
+        print str(err)
+        usage()
+
+    for o,a in opts:
+        if o in ('-h','--help'):
+            usage()
+        elif o in ('-l','--listen'):
+            listen = True
+        elif o in ('-e','--execute'):
+            execute = a
+        elif o in ('-c','--command'):
+            command = True
+        elif o in ('-u','--upload'):
+            upload_destination = a
+        elif o in ('-t','--target'):
+            target = a
+        elif o in ('-p','--port'):
+            port = int(a)
+        else:
+            print 'Invalid opts!'
+            sys.exit(0)
+
+    if not listen and len(target) and port > 0:
+        #主动连接目标端口
+        buffer = sys.stdin.read()
+        client_sender(buffer)
+
+    if listen:
+        server_loop()
+
+
+
+
+if __name__ == "__main__":
+    main()
